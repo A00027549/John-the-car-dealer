781f580104d7b1457dfd5de2be61d4a9dba16619
amethystcoder <amethystguitaristj@gmail.com>
2025-09-24 17:56:52 +0100

    progress: day one

---
+++ mainscript.js
@@ -0,0 +1,20 @@
+window.addEventListener("DOMContentLoaded",(ev)=>{
+    const images = [
+        "pic1",
+        "pic2",
+        "pic3",
+        "pic4",
+        "pic5",
+        "pic6",
+        "pic7",
+        "pic8",
+    ];
+    const header = document.querySelector("header");
+    let iterator = 0;
+    setInterval(()=>{
+
+        header.style.backgroundImage = `linear-gradient(to right, #00000083, #00000000),url(./assets/${images[iterator]}.jpg)`;
+        iterator = iterator > images.length ? 0 : iterator + 1;
+    },10000)
+
+})
